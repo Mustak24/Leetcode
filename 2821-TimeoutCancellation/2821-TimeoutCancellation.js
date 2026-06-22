d0631232bcba43f482385d6bc3de1b47771d1b14
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:49:51 +0530

    Create 2821-TimeoutCancellation

---
+++ 2821-TimeoutCancellation/2821-TimeoutCancellation.js
@@ -0,0 +1,40 @@
+// Last updated: 6/22/2026, 12:49:50 PM
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+    const myTime = setTimeout(() => {
+        fn(...args)
+    }, t);
+
+    return () => {
+        clearTimeout(myTime);
+    }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
