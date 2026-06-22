9b4d728071523942cb41a18c3ccc74e458f5d469
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:54:28 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.c
@@ -0,0 +1,6 @@
+// Last updated: 6/22/2026, 12:54:27 PM
+double myPow(double x, int n) {
+    if(n == 0) return 1;
+    if(n%2) return (n>0 ? x : (1/x))*myPow(x, (n>0 ? n-1 : n+1));
+    return myPow(x*x, n/2);
+}
