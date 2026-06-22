e44f30e169c80a33327011302064060c042d60b1
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:55:03 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.js
@@ -0,0 +1,23 @@
+// Last updated: 6/22/2026, 12:55:03 PM
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    s = s.split('')
+    let ans = []
+    let obj = {
+        '(':')',
+        '[':']',
+        '{':'}'
+    }
+    for(let i=0; i<s.length; i++){
+        if(')]}'.includes(s[i])){
+            if(obj[ans.pop()] != s[i])
+                return false
+        } else{
+            ans.push(s[i])
+        }
+    }
+    return ans.length ? false : true
+};
