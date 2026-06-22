f113668eb2cbf0a05be9a281d970c60e371841b5
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:53:21 +0530

    Create 932-MonotonicArray

---
+++ 932-MonotonicArray/932-MonotonicArray.py
@@ -0,0 +1,8 @@
+# Last updated: 6/22/2026, 12:53:21 PM
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        nums = [nums[i] for i in range(len(nums)) if nums[i-1] != nums[i] ]
+        for i in range(len(nums)-2):
+            if nums[i] < nums[i+1] > nums[i+2] : return False
+            elif nums[i] > nums[i+1] < nums[i+2] : return False
+        return True
