00e9b5fde22445f6062ab1b2e6175d112fbf643a
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:54:45 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.js
@@ -0,0 +1,9 @@
+// Last updated: 6/22/2026, 12:54:45 PM
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
