6b0432d5e816a730f5079047a193e403f1adbafb
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:48:49 +0530

    Create 3555-FinalArrayStateAfterKMultiplicationOperationsI

---
+++ 3555-FinalArrayStateAfterKMultiplicationOperationsI/3555-FinalArrayStateAfterKMultiplicationOperationsI.c
@@ -0,0 +1,19 @@
+// Last updated: 6/22/2026, 12:48:48 PM
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getFinalState(int* nums, int numsSize, int k, int multiplier, int* returnSize) {
+    *returnSize = numsSize;
+    int *newArr = (int*)malloc(sizeof(int)*numsSize);
+    
+    for(int i=0; i<k; i++){
+        int min = 0;
+        for(int j=1; j<numsSize; j++){
+            if(nums[j] < nums[min]) min = j;
+        }
+        nums[min] *= multiplier;
+    }
+
+    newArr = nums;
+    return newArr;
+}
