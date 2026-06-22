afb9310c04def39824bc17c78e18e83d3d25e7c3
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:54:36 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -0,0 +1,19 @@
+// Last updated: 6/22/2026, 12:54:35 PM
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(target < nums[0]) return 0;
+    if(target > nums[nums.length-1]) return nums.length;
+    let left = 0;
+    let rigth = nums.length-1;
+    while(left < rigth){
+        let mid = parseInt((left+rigth)/2);
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) left = mid+1;
+        else rigth = mid-1;
+    }
+    return rigth + (target > nums[rigth])
+};
