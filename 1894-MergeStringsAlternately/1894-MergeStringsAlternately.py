1145f8b35cb66784089723df7ec5fdca8fee8e88
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:52:01 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.py
@@ -0,0 +1,10 @@
+# Last updated: 6/22/2026, 12:52:01 PM
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ['' for i in range(2*len(word1) + 2*len(word2))]
+        for i in range(len(word1)): 
+            ans[2*i] = word1[i]
+        for i in range(len(word2)):
+            ans[2*i + 1] = word2[i]
+        
+        return ''.join(ans)
