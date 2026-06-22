f0bf87ee5ea7dd0346652b1308ebc073ea6884ac
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:52:40 +0530

    Create 1537-MaximumScoreAfterSplittingAString

---
+++ 1537-MaximumScoreAfterSplittingAString/1537-MaximumScoreAfterSplittingAString.c
@@ -0,0 +1,19 @@
+// Last updated: 6/22/2026, 12:52:39 PM
+int maxScore(char* s) {
+    int zero = s[0] == '0';
+    
+    int one = 0;
+    for(int i=1; s[i] != '\0'; i++){
+        if(s[i] == '1') one++;
+    }
+
+    int max = zero+one;
+    for(int i=1; s[i+1] != '\0'; i++){
+        s[i] == '0' ? zero++ : one--;
+        if(max < zero+one){
+            max = zero + one;
+        }
+    }
+
+    return max;
+}
