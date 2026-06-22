5fe3d0af616a948120f666ff246eaa0cce029b9c
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:49:41 +0530

    Create 2863-CalculatorWithMethodChaining

---
+++ 2863-CalculatorWithMethodChaining/2863-CalculatorWithMethodChaining.js
@@ -0,0 +1,62 @@
+// Last updated: 6/22/2026, 12:49:41 PM
+class Calculator {
+    
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.value = value
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.value += value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.value -= value;
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.value *= value;
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        this.value = value ? this.value/value : 'Division by zero is not allowed';
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.value = Math.pow(this.value, value);
+        return this
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.value
+    }
+}
