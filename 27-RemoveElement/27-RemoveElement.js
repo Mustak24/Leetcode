c8354661c9511f28f8af910c06a864088345e392
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:54:44 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.js
@@ -0,0 +1,14 @@
+// Last updated: 6/22/2026, 12:54:43 PM
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let index = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== val) nums[index++] = nums[i];
+    }
+
+    return index;
+};
