bd2cd3955b7195ff3d6821295f0ae77fab01a8ad
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:52:10 +0530

    Create 1786-CountTheNumberOfConsistentStrings

---
+++ 1786-CountTheNumberOfConsistentStrings/1786-CountTheNumberOfConsistentStrings.js
@@ -0,0 +1,17 @@
+// Last updated: 6/22/2026, 12:52:09 PM
+/**
+ * @param {string} allowed
+ * @param {string[]} words
+ * @return {number}
+ */
+var countConsistentStrings = function(allowed, words) {
+    let ans = 0;
+    for(let i=0; i<words.length; i++){
+        let j=0;
+        for(j; j<words[i].length; j++){
+            if(!allowed.includes(words[i][j])) break;
+        }
+        if(j==words[i].length) ans++;
+    }
+    return ans;
+};
