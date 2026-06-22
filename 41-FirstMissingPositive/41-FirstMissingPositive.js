3a780ac60832fc8a727e2d8929d22574f4914642
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:54:31 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.js
@@ -0,0 +1,14 @@
+// Last updated: 6/22/2026, 12:54:30 PM
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    if(!nums.includes(1)) return 1
+    nums = nums.sort((a,b)=>a-b);
+    for(let i=0; i<nums.length-1; i++){
+        if(nums[i+1]-nums[i] > 1 && nums[i]>0)
+            return nums[i]+1;
+    }
+    return nums[nums.length-1] + 1
+};
