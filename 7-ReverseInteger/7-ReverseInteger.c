fcad13c8279f4e1f183f566af9d2b9232af04208
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:55:17 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.c
@@ -0,0 +1,15 @@
+// Last updated: 6/22/2026, 12:55:17 PM
+int reverse(int x){
+    if(!(-2147483649 < x && x < 2147483648)) return 0;
+
+    long int ans = 0;
+    while(x){
+        int rem = x%10;
+        x /= 10;
+        ans *= 10;
+        ans += rem;
+    }
+    
+    if(-2147483649 < ans && ans < 2147483648) return ans;
+    return 0;
+}
