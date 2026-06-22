48b30ea97bc9dfa0dcad67330708e5a59141a4a0
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:50:25 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,17 @@
+// Last updated: 6/22/2026, 12:50:25 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return function() {
+        return n++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
