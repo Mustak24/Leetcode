c3d0fc22672d51574f1b5e481f8934a2ad36d27c
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:51:53 +0530

    Create 1950-SignOfTheProductOfAnArray

---
+++ 1950-SignOfTheProductOfAnArray/1950-SignOfTheProductOfAnArray.py
@@ -0,0 +1,8 @@
+# Last updated: 6/22/2026, 12:51:52 PM
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        sum = 1
+        for num in nums:
+            sum *= num
+        if sum == 0 : return sum
+        return -1 if sum < 0 else 1
