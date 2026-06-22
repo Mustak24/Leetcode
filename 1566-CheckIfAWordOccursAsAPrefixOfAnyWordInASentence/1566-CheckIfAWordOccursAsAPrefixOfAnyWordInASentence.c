6bf87adb0d044010d6f893880cb12cc39b541826
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:52:29 +0530

    Create 1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence

---
+++ 1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence/1566-CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.c
@@ -0,0 +1,25 @@
+// Last updated: 6/22/2026, 12:52:28 PM
+
+bool isPrefix(char* sen, char* pre){
+    for(int i=0; pre[i] != '\0'; i++){
+        if(sen[i] != pre[i]) return false;
+    }
+    return true;
+}
+
+
+int isPrefixOfWord(char* sentence, char* searchWord) {
+    if(isPrefix(sentence, searchWord)) return 1;
+    
+    int ans = 2;
+    for(int i=0; sentence[i] != '\0'; i++){
+        if(sentence[i] == ' '){
+            if(isPrefix((sentence + i + 1), searchWord)) {
+                return ans;
+            }
+            ans++;
+        }
+    }
+    
+    return -1;
+}
