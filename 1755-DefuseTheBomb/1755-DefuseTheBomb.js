5b74a554294b68cb8d7f62969391d503f225169a
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:52:13 +0530

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.js
@@ -0,0 +1,25 @@
+// Last updated: 6/22/2026, 12:52:13 PM
+/**
+ * @param {number[]} code
+ * @param {number} k
+ * @return {number[]}
+ */
+var decrypt = function (code, k) {
+    let ans = [];
+    let len = code.length
+    if (!k) return code.map(e => 0);
+    for (let i = 0; i < code.length; i++) {
+        let temp = [];
+        if (k > 0) {
+            for (let j = i + 1; j <= i + k; j++) {
+                temp.push(code[j < len ? j : j - len])
+            }
+        } else {
+            for(let j=i-1; j>=i+k; j--){
+                temp.push(code[j >= 0 ? j : len + j ]);
+            }
+        }
+        ans.push(temp.reduce((a, b) => a + b));
+    }
+    return ans
+};
