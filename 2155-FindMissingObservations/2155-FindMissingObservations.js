bedf13ad7bc748ffeb63d78307a96fc9fb841a1c
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:51:31 +0530

    Create 2155-FindMissingObservations

---
+++ 2155-FindMissingObservations/2155-FindMissingObservations.js
@@ -0,0 +1,22 @@
+// Last updated: 6/22/2026, 12:51:31 PM
+/**
+ * @param {number[]} rolls
+ * @param {number} mean
+ * @param {number} n
+ * @return {number[]}
+ */
+var missingRolls = function(rolls, mean, n) {
+    let x = mean*(rolls.length+n) - rolls.reduce((a,b)=>a+b,0);
+    if(x<n) return []
+    if(x>n*6) return []
+    let arr = Array.from({length: n}, (e)=>1)
+    let sum = n
+    while(true){
+        for(let i=0; i<arr.length; i++){
+            if(sum == x) return arr;
+            if(arr[i]<6)
+                arr[i] = arr[i] + 1
+                sum++
+        }
+    }
+};
