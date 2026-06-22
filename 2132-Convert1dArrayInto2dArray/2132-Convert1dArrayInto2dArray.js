f795e630d1006c83856ee2d4e001827ed8b0d3b5
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-06-22 12:51:34 +0530

    Create 2132-Convert1dArrayInto2dArray

---
+++ 2132-Convert1dArrayInto2dArray/2132-Convert1dArrayInto2dArray.js
@@ -0,0 +1,15 @@
+// Last updated: 6/22/2026, 12:51:34 PM
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if(m*n != original.length) return [];
+    let ans = []
+    for(let i=0; i<original.length; i+=n){
+        ans.push(original.slice(i,i+n))
+    }
+    return ans
+};
