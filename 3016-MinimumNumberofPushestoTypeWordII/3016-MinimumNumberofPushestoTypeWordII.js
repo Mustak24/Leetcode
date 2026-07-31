86307ccddb865baabab25ffe68b77e517328da20
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-07-31 13:23:52 +0530

    File updated at 7/31/2026, 1:23:52 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.js
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.js
@@ -1,20 +1,20 @@
-// Last updated: 7/31/2026, 1:20:39 PM
+// Last updated: 7/31/2026, 1:23:52 PM
 1/**
 2 * @param {string} word
 3 * @return {number}
 4 */
 5var minimumPushes = function(word) {
-6    const map = new Map();
+6    const counts = new Int32Array(26);
 7    for(let char of word) {
-8        map.set(char, (map.get(char) ?? 0) + 1);
+8        counts[char.charCodeAt(0) - 97] += 1
 9    }
 10
-11    const entries = Array.from(map).sort((a, b) => b[1] - a[1]);
+11   counts.sort((a, b) => b - a);
 12    
 13    let weight = 0, ans = 0;
-14    for(let i=0; i<entries.length; i++) {
+14    for(let i=0; i<26; i++) {
 15        if(i % 8 === 0) weight ++;
-16        ans += (weight * entries[i][1]);
+16        ans += (weight * counts[i]);
 17    }
 18
 19    return ans;
