00152f4709c244dba4c8461393c972c08161e202
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-03 21:30:58 +0530

    File updated at 8/3/2026, 9:30:58 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.js
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.js
@@ -1,24 +1,23 @@
-// Last updated: 8/3/2026, 9:29:56 PM
+// Last updated: 8/3/2026, 9:30:58 PM
 1/**
 2 * @param {number[]} prices
 3 * @return {number}
 4 */
 5var maxProfit = function(prices) {
-6    let profit = 0, min = prices[0], max = prices[0];
-7
-8    for(let price of prices) {
-9        if(price < max) {
-10            profit += (max - min);
-11            min = price;
-12            max = price;
-13        }
-14
-15        if(price > max) {
-16            max = price;
-17        }
-18    }
-19
-20    profit += (max - min)
-21
-22    return profit;
-23};
+6    let profit = 0;
+7    let min = prices[0], max = prices[0];
+8
+9    for(let price of prices) {
+10        if(price < max) {
+11            profit += (max - min);
+12            min = price;
+13            max = price;
+14        } else if(price > max) {
+15            max = price;
+16        }
+17    }
+18
+19    profit += (max - min)
+20
+21    return profit;
+22};
