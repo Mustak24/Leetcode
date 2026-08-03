f3029e864d0698e1c4ae92fbc3856d469d39da98
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-03 22:47:27 +0530

    File updated at 8/3/2026, 10:47:27 PM

--- 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).js
+++ 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).js
@@ -1,40 +1,49 @@
-// Last updated: 8/3/2026, 10:39:33 PM
-1
-2var RandomizedSet = function() {
-3    this.set = new Set();
-4};
-5
-6/** 
-7 * @param {number} val
-8 * @return {boolean}
-9 */
-10RandomizedSet.prototype.insert = function(val) {
-11    const has = this.set.has(val);
-12    this.set.add(val);
-13    return !has;
-14};
-15
-16/** 
-17 * @param {number} val
-18 * @return {boolean}
-19 */
-20RandomizedSet.prototype.remove = function(val) {
-21    const has = this.set.has(val);
-22    this.set.delete(val);
-23    return has;
-24};
-25
-26/**
-27 * @return {number}
-28 */
-29RandomizedSet.prototype.getRandom = function() {
-30    return Array.from(this.set)[Math.floor(this.set.size * Math.random())]
-31};
-32
-33/** 
-34 * Your RandomizedSet object will be instantiated and called as such:
-35 * var obj = new RandomizedSet()
-36 * var param_1 = obj.insert(val)
-37 * var param_2 = obj.remove(val)
-38 * var param_3 = obj.getRandom()
-39 */
+// Last updated: 8/3/2026, 10:47:27 PM
+
+var RandomizedSet = function() {
+    this.list = [];
+    this.map = new Map();
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.map.has(val)) return false;
+
+    this.list.push(val);
+    this.map.set(val, this.list.length-1);
+    return true;
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (!this.map.has(val)) return false;
+
+    let index = this.map.get(val);
+    this.list[index] = this.list[this.list.length-1];
+    this.map.set(this.list[index], index);
+    this.list.pop();
+    this.map.delete(val);
+    return true;
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    const randomIndex = Math.floor(Math.random() * this.list.length);
+    return this.list[randomIndex];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
