82b76eeb03ad3a27588062d91111c55a33c85215
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-05 23:26:11 +0530

    File updated at 8/5/2026, 11:26:11 PM

--- 135-Candy/135-Candy.js
+++ 135-Candy/135-Candy.js
@@ -1,29 +1,25 @@
-// Last updated: 6/22/2026, 12:54:03 PM
-/**
- * @param {number[]} ratings
- * @return {number}
- */
-var candy = function(ratings) {
-    let len = ratings.length;
-    let count = Array.from({length: len}, _ => 1);
-
-    let temp = Array.from({length: len}, (_, i) => [i, ratings[i]]);
-
-    temp.sort((a, b) => a[1] - b[1]);
-
-    for(let [i, e] of temp){
-        if((i < len-1 && ratings[i] < ratings[i+1]) && (i > 0 && ratings[i] < ratings[i-1])) continue;
-
-        if((i < len-1 && ratings[i] > ratings[i+1])) {
-            if(count[i] <= count[i+1])
-                count[i] = count[i+1] + 1;
-        }
-
-        if((i > 0 && ratings[i] > ratings[i-1])) {
-            if(count[i] <= count[i-1])
-                count[i] = count[i-1] + 1;
-        }
-    }
-
-    return count.reduce((a, b) => a + b)
-};
+// Last updated: 8/5/2026, 11:26:11 PM
+1/**
+2 * @param {number[]} ratings
+3 * @return {number}
+4 */
+5var candy = function(ratings) {
+6    const len = ratings.length;
+7    const child = ratings.map((r, i) => [r, i]).sort((a, b) => a[0] - b[0]);
+8
+9    const candy = new Array(len).fill(1);
+10    
+11    for(let [rating, index] of child) {
+12        const L = index - 1; const LR = ratings[L], LC = candy[L] ?? 1; 
+13        const R = index + 1; const RR = ratings[R], RC = candy[R] ?? 1;
+14
+15        if(rating < LR && rating < RR) continue;
+16
+17        candy[index] = Math.max(
+18            rating > LR ? LC : 0,
+19            rating > RR ? RC : 0,
+20        ) + 1;
+21    }
+22
+23    return candy.reduce((a, e) => a + e);
+24};
