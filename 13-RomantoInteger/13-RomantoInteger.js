56136d06cab73546ba14dc30062cfed256ce014c
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-06 20:32:39 +0530

    File updated at 8/6/2026, 8:32:38 PM

--- 13-RomantoInteger/13-RomantoInteger.js
+++ 13-RomantoInteger/13-RomantoInteger.js
@@ -1,4 +1,4 @@
-// Last updated: 8/6/2026, 8:29:41 PM
+// Last updated: 8/6/2026, 8:32:38 PM
 1/**
 2 * @param {string} s
 3 * @return {number}
@@ -13,16 +13,15 @@
 12        'D': 500, 
 13        'M': 1000
 14    }
-15
-16    const temp = [];
-17    for(let ch of s) temp.push(map[ch]);
-18
-19    let ans = temp[0];
+15    
+16    let ans = 0;
+17    for(let i=0; i<s.length; i++) {
+18        const current = map[s[i]];
+19        const next = map[s[i+1]] ?? 0;
 20
-21    for(let i=1; i<temp.length; i++) {
-22        ans += temp[i];
-23        if(temp[i] > temp[i-1]) ans -= (2 * temp[i-1]);
-24    }
-25
-26    return ans;
-27};
+21        if(current < next) ans -= current;
+22        else ans += current;
+23    }
+24
+25    return ans;
+26};
