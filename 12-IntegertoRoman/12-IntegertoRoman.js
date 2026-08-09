a88bb4b865faa3479245e90b86d9ffa55f401e64
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-09 22:01:27 +0530

    Create 12-IntegertoRoman

---
+++ 12-IntegertoRoman/12-IntegertoRoman.js
@@ -0,0 +1,22 @@
+// Last updated: 8/9/2026, 10:01:27 PM
+1/**
+2 * @param {number} num
+3 * @return {string}
+4 */
+5var intToRoman = function(num) {
+6    let ans = '';
+7
+8    const list = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']];
+9
+10    while(num) {
+11        for(let [min, size] of list) {
+12            if(num >= min) {
+13                num -= min;
+14                ans += size;
+15                break;
+16            }
+17        }
+18    }
+19
+20    return ans;
+21};
