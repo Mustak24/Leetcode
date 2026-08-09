f1f309c36b197c301c566dae2d1de27c7dfabb9c
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-09 22:24:42 +0530

    File updated at 8/9/2026, 10:24:41 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.js
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.js
@@ -1,33 +1,28 @@
-// Last updated: 6/22/2026, 12:54:59 PM
-/**
- * @param {string[]} strs
- * @return {string}
- */
-var longestCommonPrefix = function(strs) {
-    if(strs.includes("")) return "";
-    if(strs.length === 0) return "";
-    if(strs.length === 1) return strs[0];
-
-
-    let prefix = strs[0][0];
-
-    const fn = () => {
-        const index = prefix.length-1;
-        const char = prefix[index];
-
-        for(let i=1; i<strs.length; i++) {
-            const word = strs[i];
-
-            if(index >= word.length || char !== word[index]) 
-                return prefix.slice(0, index);
-        }
-
-        if(index + 1 >= strs[0].length) return prefix;
-
-        prefix += strs[0][index + 1];
-
-        return fn();
-    }
-
-    return fn();
-};
+// Last updated: 8/9/2026, 10:24:41 PM
+1/**
+2 * @param {string[]} strs
+3 * @return {string}
+4 */
+5var longestCommonPrefix = function(strs) {
+6
+7    function prefixLen(s1, s2) {
+8        const maxLen = Math.max(s1.length, s2.length);
+9
+10        let len = 0;
+11        while(len < maxLen) {
+12            if(s1[len] !== s2[len]) break;
+13            len++;
+14        }
+15
+16        return len;
+17    }
+18
+19    let prefix = strs[0];
+20    
+21    for(let i=1; i<strs.length; i++) {
+22        prefix = prefix.slice(0, prefixLen(prefix, strs[i]));
+23        if(!prefix) return prefix;
+24    }
+25
+26    return prefix
+27};
