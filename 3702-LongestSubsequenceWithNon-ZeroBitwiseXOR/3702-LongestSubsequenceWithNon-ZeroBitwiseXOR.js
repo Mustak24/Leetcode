3f92ce0e49460d69e0dd15653a938cfc710cc407
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-15 21:46:04 +0530

    File updated at 8/15/2026, 9:46:04 PM

--- 3702-LongestSubsequenceWithNon-ZeroBitwiseXOR/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR.js
+++ 3702-LongestSubsequenceWithNon-ZeroBitwiseXOR/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR.js
@@ -1,22 +1,23 @@
-// Last updated: 8/15/2026, 9:43:57 PM
+// Last updated: 8/15/2026, 9:46:04 PM
 1/**
 2 * @param {number[]} nums
 3 * @return {number}
 4 */
 5var longestSubsequence = function(nums) {
 6    const leftSum = [];
-7    let max = 0, zeros = 0;
+7    let left=0, max = 0, size=0, zeros = 0;
 8    for(let i=0; i<nums.length; i++) {
 9        if(nums[i] === 0) {
 10            zeros += 1;
 11            continue;
 12        };
 13
-14        leftSum.push(nums[i] ^ (leftSum[leftSum.length-1] ?? 0));
-15        if(leftSum[leftSum.length-1]) max = leftSum.length;
-16    }
-17
-18    if(zeros === nums.length) return 0;
-19
-20    return zeros + max
-21};
+14        left ^= nums[i];
+15        size++;
+16        if(left) max = size;
+17    }
+18
+19    if(zeros === nums.length) return 0;
+20
+21    return zeros + max
+22};
