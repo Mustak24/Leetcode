4678f22503efa416d58140b46037f0d23e896e12
Mustak khan <162583970+Mustak24@users.noreply.github.com>
2026-08-16 21:39:18 +0530

    File updated at 8/16/2026, 9:39:18 PM

--- 53-MaximumSubarray/53-MaximumSubarray.js
+++ 53-MaximumSubarray/53-MaximumSubarray.js
@@ -1,21 +1,16 @@
-// Last updated: 6/22/2026, 12:54:24 PM
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var maxSubArray = function(nums) {
-    {
-        let max = nums[0];
-        for(let num of nums) if(max < num) max = num;
-        if(max <= 0) return max; 
-    }
-
-    let max = 0;
-    let sum = 0;
-    for(let num of nums) {
-        sum += num;
-        if(max < sum) max = sum;
-        if(sum < 0) sum = 0;
-    }
-    return max;
-};
+// Last updated: 8/16/2026, 9:39:18 PM
+1/**
+2 * @param {number[]} nums
+3 * @return {number}
+4 */
+5var maxSubArray = function(nums) {
+6    let max = nums[0];
+7    let currentSum = max;
+8
+9    for(let i=1; i<nums.length; i++) {
+10        currentSum = Math.max(nums[i], currentSum + nums[i]);
+11        max = Math.max(max, currentSum);
+12    }
+13
+14    return max;
+15};
